4d378394b206f46fbe7b3d9053333c693a4c8fe4
Sujallalawat <106472593+Sujallalawat@users.noreply.github.com>
2023-07-02 12:19:14 +0530

    assign2

---
+++ Assignmnet-2/Assignment-2.cpp
@@ -0,0 +1,46 @@
+/*
+1. Using inheritance, one class can acquire property of others. Consider the following animal class:-
+class Animal()
+{
+    public:
+    void walk()
+    {
+        cout<<"I am walking"<<endl;
+    }
+};
+implement the Bird class which inherits the animal class and implement the flying function in it.
+Input:-
+bird.walk();
+bird.fly();
+
+Output:-
+I am walking
+I am flying
+*/
+
+
+//code your solution here(You may increase the space and work efficiently and properly)
+
+
+
+
+/*
+2. When a subclass inherits from a superclass, it will also inherits its methods however, it can 
+also overrides the superclass methods(as well as declare and implement new one).
+Consider sports class:-
+
+class Sports
+{
+    public:
+    string getname()
+    {
+        return "all sports";
+    }
+    void getTeamMembers()
+    {
+        cout<<"Each team has n player in "+ getname()<<endl;
+    }
+};
+*/
+
+
